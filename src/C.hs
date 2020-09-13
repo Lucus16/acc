e31a209ddb2790b15db6924c46e5eaa1be9f10b8
Lars Jellema <lars.jellema@gmail.com>
2020-09-13 13:44:38 +0200

    Support binary operations

--- src/C.hs
+++ src/C.hs
@@ -2,8 +2,11 @@
 
 module C where
 
+import Prelude hiding (product, sum)
+
 import Control.Applicative ((<|>))
 import Data.Char (isAlpha, isDigit, isSpace)
+import Data.Function ((&))
 import Data.Functor (void)
 import Data.Text (Text)
 import Data.Void (Void)
@@ -33,11 +36,23 @@ data Statement
   = Return Expression
   deriving (Show)
 
+data Unary
+  = Inv
+  | Neg
+  | Not
+  deriving (Show)
+
+data Binary
+  = Add
+  | Sub
+  | Mul
+  | Div
+  deriving (Show)
+
 data Expression
   = Term Term
-  | Negate Expression
-  | Invert Expression
-  | Not Expression
+  | Unary Unary Expression
+  | Binary Binary Expression Expression
   deriving (Show)
 
 data Term
@@ -76,7 +91,7 @@ symbol :: Text -> Parser Text
 symbol = L.symbol space
 
 integer :: Parser Literal
-integer = Integer <$> L.decimal
+integer = Integer <$> lexeme L.decimal
 
 literal :: Parser Literal
 literal = integer
@@ -84,11 +99,42 @@ literal = integer
 term :: Parser Term
 term = Literal <$> literal
 
+parenthesized :: Parser a -> Parser a
+parenthesized = between (symbol "(") (symbol ")")
+
+binaryText :: Binary -> Text
+binaryText Add = "+"
+binaryText Sub = "-"
+binaryText Mul = "*"
+binaryText Div = "/"
+
+trailingBinary :: Binary -> Parser Expression -> Parser (Expression -> Expression)
+trailingBinary op sub = symbol (binaryText op) *> fmap (flip $ Binary op) sub
+
+binarySequence :: Parser Expression -> Parser (Expression -> Expression) -> Parser Expression
+binarySequence first rest = foldl (&) <$> first <*> many rest
+
+sum :: Parser Expression
+sum = binarySequence product (addition <|> subtraction)
+  where
+    addition    = trailingBinary Add product
+    subtraction = trailingBinary Sub product
+
+product :: Parser Expression
+product = binarySequence unary (multiplication <|> division)
+  where
+    multiplication = trailingBinary Mul unary
+    division       = trailingBinary Div unary
+
 expression :: Parser Expression
-expression = Term <$> term
-  <|> Negate <$> (symbol "-" *> expression)
-  <|> Invert <$> (symbol "~" *> expression)
-  <|> Not <$> (symbol "!" *> expression)
+expression = sum
+
+unary :: Parser Expression
+unary = Term <$> term
+  <|> Unary Neg <$> (symbol "-" *> unary)
+  <|> Unary Inv <$> (symbol "~" *> unary)
+  <|> Unary Not <$> (symbol "!" *> unary)
+  <|> parenthesized expression
 
 returnStatement :: Parser Statement
 returnStatement = fmap Return $ symbol "return" *> expression <* symbol ";"
@@ -103,7 +149,7 @@ type_ :: Parser Type
 type_ = symbol "int"
 
 parameters :: Parser Parameters
-parameters = between (symbol "(") (symbol ")") $ pure []
+parameters = parenthesized $ pure []
 
 fdef :: Parser FunctionDefinition
 fdef = Fdef <$> type_ <*> identifier <*> parameters <*> body
@@ -118,13 +164,40 @@ instance Assembly FunctionDefinition where
 instance Assembly Expression where
   assembly (Term (Literal (Integer i)))
     = [ "movq $" <> tshow i <> ", %rax" ]
-  assembly (Negate e) = assembly e <> [ "neg %rax" ]
-  assembly (Invert e) = assembly e <> [ "not %rax" ]
-  assembly (Not e) = assembly e
-    <> [ "cmpq $0, %rax"
-       , "movq $0, %rax"
-       , "sete %al"
-       ]
+
+  assembly (Unary op e) = assembly e <> assembly op
+
+  assembly (Binary op l r)
+    = case (unaryAssembly "rax" l, unaryAssembly "rcx" r) of
+        (_, Just rasm) -> assembly l <> rasm <> assembly op
+        (Just lasm, _) -> assembly r <> [ "movq %rax, %rcx" ] <> lasm <> assembly op
+        (Nothing, Nothing) ->
+          assembly r <> [ "push %rax" ]
+            <> assembly l <> [ "pop %rcx" ]
+            <> assembly op
+  where
+    unaryAssembly reg (Binary _ _ _) = Nothing
+    unaryAssembly reg (Term (Literal (Integer i)))
+      = Just [ "movq $" <> tshow i <> ", %" <> reg ]
+
+    unaryAssembly reg (Unary Neg e) = (<>["neg %" <> reg]) <$> unaryAssembly reg e
+    unaryAssembly reg (Unary Inv e) = (<>["not %" <> reg]) <$> unaryAssembly reg e
+    unaryAssembly reg (Unary Not e) = Nothing
+
+instance Assembly Unary where
+  assembly Neg = [ "neg %rax" ]
+  assembly Inv = [ "not %rax" ]
+  assembly Not
+    = [ "cmpq $0, %rax"
+      , "movq $0, %rax"
+      , "sete %al"
+      ]
+
+instance Assembly Binary where
+  assembly Div = [ "cqo", "idivq %rcx" ]
+  assembly Mul = [ "imul %rcx" ]
+  assembly Add = [ "add %rcx, %rax" ]
+  assembly Sub = [ "sub %rcx, %rax" ]
 
 instance Assembly Statement where
   assembly (Return e) = assembly e <> [ "ret" ]
