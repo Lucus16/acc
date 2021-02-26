75d108ec0b1f60be09c829906dd7b1acf6a34534
Lars Jellema <lars.jellema@gmail.com>
2021-02-26 17:04:44 +0100

    Add dependencies to shell

--- shell.nix
+++ shell.nix
@@ -2,6 +2,17 @@
 
 with pkgs;
 
-mkShell {
-  buildInputs = [ cabal-install ghc ghcid hlint ];
+let
+  ghc = haskell.packages.ghc8103.ghcWithHoogle (h: with h; [
+    containers
+    filepath
+    megaparsec
+    mtl
+    parser-combinators
+    process
+    text
+  ]);
+
+in mkShell {
+  buildInputs = [ cabal-install ghc ghcid haskell-language-server hlint ];
 }
