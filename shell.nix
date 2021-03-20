36a6009b167f7e62686050104c23754f7e10cacd
Lars Jellema <lars.jellema@gmail.com>
2021-03-20 18:51:57 +0100

    Remove ghc version override

--- shell.nix
+++ shell.nix
@@ -3,15 +3,16 @@
 with pkgs;
 
 let
-  ghc = haskell.packages.ghc8103.ghcWithHoogle (h: with h; [
-    containers
-    filepath
-    megaparsec
-    mtl
-    parser-combinators
-    process
-    text
-  ]);
+  ghc = haskellPackages.ghcWithHoogle (h:
+    with h; [
+      containers
+      filepath
+      megaparsec
+      mtl
+      parser-combinators
+      process
+      text
+    ]);
 
 in mkShell {
   buildInputs = [ cabal-install ghc ghcid haskell-language-server hlint ];
