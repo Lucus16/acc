440cb969b35493be3412507e740473cd6d75fa65
Lars Jellema <lars.jellema@gmail.com>
2022-12-02 00:16:40 +0100

    Elf: Add symbol table for objdump

--- src/Elf.hs
+++ src/Elf.hs
@@ -46,8 +46,22 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
 
   -- section header table
   sectionTableOffset <- Binary.getOffset
+
+  -- empty section header needed to indicate the names section index is not
+  -- insanely high
+  Binary.word32 0 -- section name offset
+  Binary.word32 0 -- section header type: symbol table
+  Binary.word64 0 -- flags, unused
+  Binary.word64 0 -- address, unused
+  Binary.word64 0 -- section offset
+  Binary.word64 0 -- section size
+  Binary.word32 0 -- associated section index
+  Binary.word32 0 -- extra section info
+  Binary.word64 0 -- required section alignment
+  Binary.word64 0 -- entry size if entries are fixed-size
+
   -- names section header
-  let namesSectionIndex = 0
+  let namesSectionIndex = 1
   Binary.word32 namesNameOffset -- section name offset
   Binary.word32 3 -- section header type: string table
   Binary.word64 0 -- flags, unused
@@ -58,7 +72,21 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
   Binary.word32 0 -- extra section info
   Binary.word64 0 -- required section alignment
   Binary.word64 0 -- entry size if entries are fixed-size
+
+  -- symbol table section header
+  Binary.word32 symbolsNameOffset -- section name offset
+  Binary.word32 2 -- section header type: symbol table
+  Binary.word64 0 -- flags, unused
+  Binary.word64 0 -- address, unused
+  Binary.word64 symbolsSectionOffset
+  Binary.word64 symbolsSectionSize
+  Binary.word32 0 -- associated section index
+  Binary.word32 0 -- extra section info
+  Binary.word64 0 -- required section alignment
+  Binary.word64 24 -- entry size if entries are fixed-size
+
   -- code section header
+  let codeSectionIndex = 3
   Binary.word32 codeNameOffset
   Binary.word32 1 -- section header type: program
   Binary.word64 6 -- flags: runtime_allocated | executable
@@ -69,6 +97,7 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
   Binary.word32 0 -- extra section info
   Binary.word64 0 -- required section alignment
   Binary.word64 0 -- entry size if entries are fixed-size
+
   -- rodata section header
   Binary.word32 rodataNameOffset
   Binary.word32 1 -- section header type: program
@@ -80,7 +109,7 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
   Binary.word32 0 -- extra section info
   Binary.word64 0 -- required section alignment
   Binary.word64 0 -- entry size if entries are fixed-size
-  let sectionHeaderCount = 3
+  let sectionHeaderCount = 5
 
   -- segment header table
   programTableOffset <- Binary.getOffset
@@ -94,6 +123,16 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
   Binary.word64 mainSegmentSize -- number of bytes to clear before copying
   Binary.word64 alignment
 
+  -- symbol table section
+  symbolsSectionOffset <- Binary.getOffset
+  Binary.word32 mainFunctionNameOffset -- symbol name
+  Binary.word8 0x12 -- symbol type: function, symbol binding: global
+  Binary.word8 0 -- symbol visibility: public
+  Binary.word16 codeSectionIndex -- related section header index
+  Binary.word64 virtualMemoryOffset -- virtual address of symbol
+  Binary.word64 codeSectionSize -- size of symbol code
+  symbolsSectionSize <- subtract symbolsSectionOffset <$> Binary.getOffset
+
   -- names section
   namesSectionOffset <- Binary.getOffset
   namesNameOffset <- fromIntegral . subtract namesSectionOffset <$> Binary.getOffset
@@ -102,6 +141,10 @@ build Simple{code, rodata} = BSB.toLazyByteString $ Binary.execWriter mdo
   Binary.cString ".text"
   rodataNameOffset <- fromIntegral . subtract namesSectionOffset <$> Binary.getOffset
   Binary.cString ".rodata"
+  symbolsNameOffset <- fromIntegral . subtract namesSectionOffset <$> Binary.getOffset
+  Binary.cString ".symtab"
+  mainFunctionNameOffset <- fromIntegral . subtract namesSectionOffset <$> Binary.getOffset
+  Binary.cString "main"
   namesSectionSize <- subtract namesSectionOffset <$> Binary.getOffset
 
   -- Make sure the data that needs to be mapped at runtime is aligned with the
